31adabcc311e2ec2e6268d7d15fa769d8391a96f
Efrat Friedman <efratfriedman18@gmail.com>
2025-10-30 11:29:41 +0200

    arrange the types in cart state

--- src/types/CartState.ts
+++ src/types/CartState.ts
@@ -1,12 +1,12 @@
 import { Product } from "./Product";
 
 export default interface CartState {
-  products: Product[];
-  cartCount: number;
-  addItem: (product: Product) => void;
-  removeItem: (id: number) => void;
-  deleteItem: (id: number) => void;
-  resetCart: () => void;
-  setProducts: (products: Product[]) => void;
-  sum: number;
+    products: Product[];
+    cartCount: number;
+    sum: number;
+    addItem: (product: Product) => void;
+    removeItem: (id: number) => void;
+    deleteItem: (id: number) => void;
+    resetCart: () => void;
+    setProducts: (products: Product[]) => void;
 }
